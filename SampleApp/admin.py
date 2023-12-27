49572be8964d0af74e1347a7375683147280ab5e
brayokenya <kiirubrian21@gmail.com>
2023-12-27 23:44:11 +0300

    added styles to templates

--- SampleApp/admin.py
+++ SampleApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Collection, Sample
 
 # Register your models here.
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'disease_term', 'title')
+
+@admin.register(Sample)
+class SampleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'collection', 'user', 'donor_count', 'material_type', 'last_updated')
--- SampleApp/urls.py
+++ SampleApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import SignUpView
-
-
-
+from .views import SignUpView, ProfileView, CollectionListView, CollectionDetailView, SampleCreateView, CollectionCreateView
 
 urlpatterns = [
+    path('dashboard/', CollectionListView.as_view(), name='home'),
     path("signup/", SignUpView.as_view(), name="signup"),
-    
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('collection/<int:pk>/', CollectionDetailView.as_view(), name='collection_details'),
+    path('collection/<int:collection_id>/add_sample/', SampleCreateView.as_view(), name='add_sample'),
+    path('create_collection/', CollectionCreateView.as_view(), name='create_collection'),
 ]
