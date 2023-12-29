bba162f5dc29f0e212d616297feff365325d3211
brayokenya <kiirubrian21@gmail.com>
2023-12-29 13:57:35 +0300

    added custom 404 page

--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&6@)=!@9@v+*9=6+_yr+ll2@$t@e(jpp1*dn7+-*-3-92a9(bg"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
